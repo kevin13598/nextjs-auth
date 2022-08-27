a384ebf4e7cf328a41dc2c728fb27c66e82caa73
Kevin <kevin.lam13@hotmail.com>
2022-08-27 14:50:03 -0600

    first commit

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import classes from "./main-navigation.module.css";
+
+function MainNavigation() {
+  const { data: session, status } = useSession();
+
+  function logoutHandler() {
+    signOut();
+  }
+
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <a>
+          <div className={classes.logo}>Next Auth</div>
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          {!session && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
